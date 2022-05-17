04c0de132e2239ad9c49a43db6999eefe26b7444
romanbatiuk <roman.batiuk87@gmail.com>
2022-05-17 20:23:29 +0300

    data validation

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserRegisterDto {
+	@IsString({ message: 'Name cannot be empty' })
 	name: string;
+
+	@IsEmail({}, { message: 'Email is incorrect' })
 	email: string;
+
+	@IsString({ message: 'Password cannot be empty' })
 	password: string;
 }
